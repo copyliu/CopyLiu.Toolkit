9eef720e2a6946f421d3c230ac2006cad0c50305
Copy Liu <copyliu@gmail.com>
2022-06-15 23:35:36 +0800

    hash 调整, 加入 string

--- CopyLiu.Toolkit/Hash/Hash.cs
+++ CopyLiu.Toolkit/Hash/Hash.cs
@@ -1,49 +1,46 @@
-﻿using System.Linq;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace CopyLiu.Toolkit.Hash
 {
     public class HashHelper
     {
-        private static string CalcHash(byte[] input, HashAlgorithm hashAlgorithm)
+        private static byte[] ComputeHash(byte[] input, HashAlgorithm hashAlgorithm)
         {
-            return string.Join("", hashAlgorithm
-                .ComputeHash(input)
-                .Select(p => p.ToString("x2")));
+            return hashAlgorithm.ComputeHash(input);
         }
 
-        public static string MD5(byte[] input)
+        public static byte[] MD5(byte[] input)
         {
             using var hash = System.Security.Cryptography.MD5.Create();
-            return CalcHash(input, hash);
+            return ComputeHash(input, hash);
         }
 
-        public static string MD5(string input, Encoding encoding = null)
+        public static byte[] MD5(string input, Encoding encoding = null)
         {
             encoding ??= Encoding.UTF8;
             return MD5(encoding.GetBytes(input));
         }
 
-        public static string SHA1(byte[] input)
+        public static byte[] SHA1(byte[] input)
         {
             using var hash = System.Security.Cryptography.SHA1.Create();
-            return CalcHash(input, hash);
+            return ComputeHash(input, hash);
         }
 
-        public static string SHA1(string input, Encoding encoding = null)
+        public static byte[] SHA1(string input, Encoding encoding = null)
         {
             encoding ??= Encoding.UTF8;
             return SHA1(encoding.GetBytes(input));
         }
 
-        public static string SHA256(byte[] input)
+        public static byte[] SHA256(byte[] input)
         {
             using var hash = System.Security.Cryptography.SHA256.Create();
-            return CalcHash(input, hash);
+            return ComputeHash(input, hash);
         }
 
-        public static string SHA256(string input, Encoding encoding = null)
+        public static byte[] SHA256(string input, Encoding encoding = null)
         {
             encoding ??= Encoding.UTF8;
             return SHA256(encoding.GetBytes(input));
---
+++ CopyLiu.Toolkit/String/Extensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace CopyLiu.Toolkit.String
+{
+    public static class Extensions
+    {
+        private static char GetHexValue(int i)
+        {
+            return i < 10 ? (char)(i + 48) : (char)(i - 10 + 65);
+        }
+
+        public static string ToHexString(this byte[] input)
+        {
+#if NETFRAMEWORK || NETSTANDARD2_0
+            char[] chArray = new char[input.Length*2];
+            var arrayIndex = 0;
+            for (int index = 0; index < input.Length*2; index += 2)
+            {
+                byte num2 = input[arrayIndex++];
+                chArray[index] = GetHexValue((int) num2 / 16);
+                chArray[index + 1] = GetHexValue((int) num2 % 16);
+            }
+
+            return new string(chArray);
+#elif NETSTANDARD2_1
+            var length = input.Length;
+            BitConverter.ToString(input);
+            return string.Create(length * 2, input, (dst, state) =>
+            {
+                const string HexValues = "0123456789abcdef";
+
+                var src = new ReadOnlySpan<byte>(state);
+
+                var i = 0;
+                var j = 0;
+
+                var b = src[i++];
+                dst[j++] = HexValues[b >> 4];
+                dst[j++] = HexValues[b & 0xF];
+
+                while (i < src.Length)
+                {
+                    b = src[i++];
+                    dst[j++] = HexValues[b >> 4];
+                    dst[j++] = HexValues[b & 0xF];
+                }
+            });
+#else
+            return string.Join("", input.Select(p => p.ToString("x2")));
+#endif
+        }
+
+        public static byte[] AsByteArray(this string hexString)
+        {
+            //TODO: 性能优化
+            return Enumerable.Range(0, hexString.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hexString.Substring(x, 2), 16))
+                .ToArray();
+        }
+    }
+}
